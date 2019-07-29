6283fa728afbe1d0b0126c934914746dc0c40530
Sun Changming <cm.sun1982@outlook.com>
2019-07-29 23:27:07 +0900

    ds commit

--- src/main/java/com/example/demo/api/domain/service/UserService.java
+++ src/main/java/com/example/demo/api/domain/service/UserService.java
@@ -1,9 +1,30 @@
 package com.example.demo.api.domain.service;
 
+import com.example.demo.api.infra.mapper.BudgetMapper;
+import com.example.demo.api.infra.mapper.ShoppingMapper;
+import com.example.demo.api.infra.mapper.UserMapper;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
-public class UserService{
+public class UserService {
+  @NonNull private final UserMapper userMapper;
+
+  @NonNull private final BudgetMapper budgetMapper;
+
+  @NonNull private final ShoppingMapper shoppingMapper;
+
+  @Transactional(rollbackFor = {Exception.class})
+  public void modifyUserBy() {
+    userMapper.selectUser();
+
+    budgetMapper.updateBudget();
+
+    shoppingMapper.updateShopping();
+  }
 }
